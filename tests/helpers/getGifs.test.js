fd550f696f42a65334693accb70f7d6bcf6f66ce
Miguel <Miguel.giordano@solera.com>
2023-08-08 15:41:19 +0200

    getGifs test

---
+++ tests/helpers/getGifs.test.js
@@ -0,0 +1,16 @@
+import { getGif } from '../../src/helpers/getGifs'
+
+describe('Prueba en getGifs()', () => { 
+
+    test('debe retornar un arreglo de gifs', async() => { 
+
+        const gifs = await getGif('One Punch')
+        //console.log(gifs)
+        expect( gifs.length ).toBeGreaterThan(0)
+        expect( gifs[0] ).toEqual({
+            id: expect.any( String ),
+            title: expect.any( String ),
+            url: expect.any( String ),
+        })
+    })
+})
